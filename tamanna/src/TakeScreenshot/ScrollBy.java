74ffe60f3d1df1ba9a66f300046a3c8ea71184cc
sahilbhaldar <sahil.bhaldar09@gmail.com>
2023-07-04 17:02:41 +0530

    This is my 4th commit

---
+++ tamanna/src/TakeScreenshot/ScrollBy.java
@@ -0,0 +1,18 @@
+package TakeScreenshot;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollBy {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.actimind.com");
+    JavascriptExecutor jse=(JavascriptExecutor) driver;
+    for(int i=0;i<=1000;i++)
+    jse.executeScript("window.scrollBy(0,2)");
+    for(int i=0;i<=1000;i++)
+        jse.executeScript("window.scrollBy(0,-2)");
+}
+}
